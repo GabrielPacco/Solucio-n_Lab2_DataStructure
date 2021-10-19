57db0653ab5c2a2f6d499081997730b32d434081
GabrielPacco <gpaccohu@unsa.edu.pe>
2021-10-19 01:39:10 -0500

    Initial Commit

---
+++ Solución_Lab2_DataStructure/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Solución_Lab2_DataStructure
+//
+//  Created by Gabriel on 19/10/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
